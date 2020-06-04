3fd8639768bfa03bd80bfac32ce4ca6385df76cf
steve <hawks>
2020-06-04 18:04:26 +0800

    验证已经写好了   实现了登陆

--- PersonRecognition.py
+++ PersonRecognition.py
@@ -12,11 +12,12 @@ driver = webdriver.Chrome()
 def loadQcode():
     driver.get("https://wsmusic.sounm.com/unlock")
     img_url = driver.find_element_by_css_selector("img")
-    url = img_url.get_attribute("src")
+    unlockUrl = img_url.get_attribute("src")
     # img_name = url.split('/')[-1:]
 
-    print(url)
-    return url
+    print(unlockUrl)
+    return unlockUrl
+
 
 # 定义下载函数
 def downLoad(url):
@@ -24,20 +25,31 @@ def downLoad(url):
     # 获取的文本实际上是图片的二进制文本
     img = response.content
     # 将他拷贝到本地文件 w 写  b 二进制  wb代表写入二进制文本
-    with open('./a.jpg', 'wb') as f:
+    with open('./img/a.jpg', 'wb') as f:
         f.write(img)
 
 
-
+# 识别二维码
 def scanQcode():
-    # 识别二维码
     reader = zxing.BarCodeReader()
-    # barcode = reader.decode("https://cdn.dnpw.org/api/qrcode/cache/fecb8b7504959d5a7f4aff400f2db9c3.png_300_300_2_95.jpg")
-    # print(barcode.parsed)
+    barcode = reader.decode("./img/a.jpg")
+    code = barcode.parsed
+
+    return code[-4:]
+
+
+# 填写解析过的二维码，并且输入点击跳转到index
+def inputQcode(codeVan):
+    vanInput = driver.find_element_by_css_selector("input.van-field__control")
+    vanInput.send_keys(codeVan)
+    # 点击发送
+    driver.find_element_by_css_selector("button").click()
 
 
 if __name__ == '__main__':
     url = loadQcode()
     downLoad(url)
+    code = scanQcode()
+    inputQcode(code)
     time.sleep(2)
-    driver.quit()
+    #driver.quit()
