d5b4ee878cc31276774872c461ac076ba5873d88
tunogya <tunogya@qq.com>
2020-06-13 15:16:31 +0800

    删除PyQt5

--- scraper.py
+++ scraper.py
@@ -1,13 +1,4 @@
 from Sounimei import Sounimei
-from GUI_main import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication
-import sys
-
-
-class MyWin(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(MyWin, self).__init__(parent)
-        self.setupUi(self)
 
 
 if __name__ == '__main__':
