ff9010cbccd4c036114354879ad21d0d2254bf80
tunogya <tunogya@qq.com>
2020-06-14 10:03:01 +0800

    删除残余GUI代码

--- scraper.py
+++ scraper.py
@@ -2,10 +2,6 @@ from Sounimei import Sounimei
 
 
 if __name__ == '__main__':
-    # app = QApplication(sys.argv)
-    # mw = MyWin()
-    # mw.show()
-    # sys.exit(app.exec())
     spider = Sounimei()
     spider.run()
     # spider.collection()
