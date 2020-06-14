c69d57a2bc0276ab3bd657f60a69a0c12f3ee38d
tunogya <tunogya@qq.com>
2020-06-14 10:08:29 +0800

    解决SQL错误:SQL中含'的情况

--- connectDB.py
+++ connectDB.py
@@ -41,12 +41,13 @@ def my_insert_result(result):
     try:
         title = result['title'].replace('\'', '"')
         singer = result['singer'].replace('\'', '"')
-        album = result['album']
+        album = result['album'].replace('\'', '"')
         file_name = result['file_name']
         url = result['url']
         img = result['img']
         sql = "INSERT INTO `qq_music` (`title`, `singer`, `album`, `file_name`,`img`, `url`) VALUE ('" + \
               title + "','" + singer + "','" + album + "','" + file_name + "','" + img + "','" + url + "');"
+        print(sql)
         cursor.execute(sql)
         db.commit()
         print("《" + title + "》Write Successful!")
